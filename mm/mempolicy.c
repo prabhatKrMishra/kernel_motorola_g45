1954a0bb3e5b9f9be242b207eb7679fcb696beae
Wenchao Hao <haowenchao22@gmail.com>
2020-08-11 18:31:16 -0700

    mm/mempolicy.c: check parameters first in kernel_get_mempolicy
    Previous implementatoin calls untagged_addr() before error check, while if
    the error check failed and return EINVAL, the untagged_addr() call is just
    useless work.
    
    Signed-off-by: Wenchao Hao <haowenchao22@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Reviewed-by: Andrew Morton <akpm@linux-foundation.org>
    Link: http://lkml.kernel.org/r/20200801090825.5597-1-haowenchao22@gmail.com
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: prabhatkrmishra <mprabhat774@gmail.com>

--- mm/mempolicy.c
+++ mm/mempolicy.c
@@ -1569,10 +1569,10 @@ static int kernel_get_mempolicy(int __user *policy,
 	int pval;
 	nodemask_t nodes;
 
-	addr = untagged_addr(addr);
-
 	if (nmask != NULL && maxnode < nr_node_ids)
 		return -EINVAL;
+
+	addr = untagged_addr(addr);
 
 	err = do_get_mempolicy(&pval, &nodes, addr, flags);
 
