c8789030e4e2241afa44d0295ff272d7698e0f08
Paul E. McKenney <paulmck@kernel.org>
2021-12-06 15:12:14 -0800

    rcutorture: Print message before invoking ->cb_barrier()
    The various ->cb_barrier() functions, for example, rcu_barrier(),
    sometimes cause rcutorture hangs.  But currently, the last console
    message is the unenlightening "Stopping rcu_torture_stats".  This commit
    therefore prints a message of the form "rcu_torture_cleanup: Invoking
    rcu_barrier+0x0/0x1e0()" to help point people in the right direction.
    
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>
    Signed-off-by: prabhatkrmishra <mprabhat774@gmail.com>

--- kernel/rcu/rcutorture.c
+++ kernel/rcu/rcutorture.c
@@ -2911,8 +2911,10 @@ rcu_torture_cleanup(void)
 	int i;
 
 	if (torture_cleanup_begin()) {
-		if (cur_ops->cb_barrier != NULL)
+		if (cur_ops->cb_barrier != NULL) {
+			pr_info("%s: Invoking %pS().\n", __func__, cur_ops->cb_barrier);
 			cur_ops->cb_barrier();
+		}
 		return;
 	}
 	if (!cur_ops) {
@@ -2967,8 +2969,10 @@ rcu_torture_cleanup(void)
 	 * Wait for all RCU callbacks to fire, then do torture-type-specific
 	 * cleanup operations.
 	 */
-	if (cur_ops->cb_barrier != NULL)
+	if (cur_ops->cb_barrier != NULL) {
+		pr_info("%s: Invoking %pS().\n", __func__, cur_ops->cb_barrier);
 		cur_ops->cb_barrier();
+	}
 	if (cur_ops->cleanup != NULL)
 		cur_ops->cleanup();
 
