873fd4646b0eb5da30f04af931e6647bbefe589e
Shaoying Xu <shaoyi@amazon.com>
2025-04-17 18:20:00 +0000

    mm: change default tuneable sysctl_page_lock_unfairness to 0
    We observed 7%-8% regression in FIO randwrite test with multiple
    threads after this commit ("mm: allow a controlled amount of unfairness
    in the page lock") which set page_lock_unfairness default to 5.
    After tuning it to 0, the performance can be improved back.
    
    Signed-off-by: Shaoying Xu <shaoyi@amazon.com>

--- mm/filemap.c
+++ mm/filemap.c
@@ -1242,7 +1242,7 @@ static inline bool trylock_page_bit_common(struct page *page, int bit_nr,
 }
 
 /* How many times do we accept lock stealing from under a waiter? */
-int sysctl_page_lock_unfairness = 5;
+int sysctl_page_lock_unfairness = 0;
 
 static inline int wait_on_page_bit_common(wait_queue_head_t *q,
 	struct page *page, int bit_nr, int state, enum behavior behavior)
