3dc0ec1c3b8b4f14a633ad2981257c3002ba1534
huangzq2 <huangzq2@motorola.com>
2025-04-18 11:08:08 +0000

    drivers/moto_swap: Refactor moto_swap
    - Disable lru shrinker in mswapd by default.
    - Remove unused code.
    - Increase moto_swap version to 3.
    
    Change-Id: I22513a4fc1d50ff5db30675fd481b55cf8c59e49
    Signed-off-by: huangzq2 <huangzq2@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2996467
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Wang Wang <wangwang1@lenovo.com>
    Submit-Approved: Jira Key
    Signed-off-by: prabhatKrMishra <mprabhat774@gmail.com>

--- drivers/moto_swap/hybridswap/hybridswap_main.c
+++ drivers/moto_swap/hybridswap/hybridswap_main.c
@@ -62,13 +62,6 @@ static const char *swapd_text[NR_EVENT_ITEMS] = {
 	"swapd_snapshot_times",
 	"swapd_skip_shrink_of_window",
 	"swapd_manual_pause",
-#ifdef CONFIG_OPLUS_JANK
-	"swapd_cpu_busy_skip_times",
-	"swapd_cpu_busy_break_times",
-#endif
-#endif
-#ifdef CONFIG_HYBRIDSWAP_ASYNC_COMPRESS
-	"akcompressd_running",
 #endif
 };
 
@@ -106,19 +99,6 @@ int hybridswap_loglevel(void)
 
 void __put_memcg_cache(memcg_hybs_t *hybs)
 {
-#ifdef CONFIG_HYBRIDSWAP_ASYNC_COMPRESS
-	if (hybs->cache.id > 0) {
-		spin_lock(&cached_idr_lock);
-		idr_replace(&cached_idr, NULL, hybs->cache.id);
-		idr_remove(&cached_idr, hybs->cache.id);
-		spin_unlock(&cached_idr_lock);
-	}
-
-	spin_lock(&hybs->cache.lock);
-	if (hybs->cache.dead != 1)
-		BUG();
-	spin_unlock(&hybs->cache.lock);
-#endif
 	kmem_cache_free(hybridswap_cache, (void *)hybs);
 }
 
@@ -230,23 +210,6 @@ memcg_hybs_t *hybridswap_cache_alloc(struct mem_cgroup *memcg, bool atomic)
 	if (!hybs)
 		return NULL;
 
-#ifdef CONFIG_HYBRIDSWAP_ASYNC_COMPRESS
-	spin_lock_init(&hybs->cache.lock);
-	INIT_LIST_HEAD(&hybs->cache.head);
-	hybs->cache.cnt = 0;
-	hybs->cache.compressing = 0;
-	hybs->cache.dead = 0;
-	spin_lock(&cached_idr_lock);
-	hybs->cache.id = idr_alloc(&cached_idr, NULL, 1, MEM_CGROUP_ID_MAX,
-			GFP_KERNEL);
-	if (hybs->cache.id < 0) {
-		spin_unlock(&cached_idr_lock);
-		kmem_cache_free(hybridswap_cache, (void*)hybs);
-		return NULL;
-	}
-	idr_replace(&cached_idr, &hybs->cache, hybs->cache.id);
-	spin_unlock(&cached_idr_lock);
-#endif
 	INIT_LIST_HEAD(&hybs->grade_node);
 #ifdef CONFIG_HYBRIDSWAP_CORE
 	spin_lock_init(&hybs->zram_init_lock);
@@ -263,9 +226,6 @@ memcg_hybs_t *hybridswap_cache_alloc(struct mem_cgroup *memcg, bool atomic)
 
 	ret = atomic64_cmpxchg((atomic64_t *)&MEMCG_OEM_DATA(memcg), 0, (u64)hybs);
 	if (ret != 0) {
-#ifdef CONFIG_HYBRIDSWAP_ASYNC_COMPRESS
-		hybs->cache.dead = 1;
-#endif
 		put_memcg_cache(hybs);
 		return (memcg_hybs_t *)ret;
 	}
@@ -330,9 +290,6 @@ static void mem_cgroup_free_hook(void *data, struct mem_cgroup *memcg)
 
 	hybs = (memcg_hybs_t *)MEMCG_OEM_DATA(memcg);
 	MEMCG_OEM_DATA(memcg) = 0;
-#ifdef CONFIG_HYBRIDSWAP_ASYNC_COMPRESS
-	clear_page_memcg(&hybs->cache);
-#endif
 	put_memcg_cache(hybs);
 }
 
@@ -924,12 +881,6 @@ static int hybridswap_enable(struct zram *zram)
 		return ret;
 #endif
 
-#ifdef CONFIG_HYBRIDSWAP_ASYNC_COMPRESS
-	ret = create_akcompressd_task(zram);
-	if (ret)
-		goto create_akcompressd_task_fail;
-#endif
-
 #ifdef CONFIG_HYBRIDSWAP_CORE
 	ret = hybridswap_core_enable();
 	if (ret)
@@ -941,10 +892,6 @@ static int hybridswap_enable(struct zram *zram)
 
 #ifdef CONFIG_HYBRIDSWAP_CORE
 hybridswap_core_enable_fail:
-#endif
-#ifdef CONFIG_HYBRIDSWAP_ASYNC_COMPRESS
-	destroy_akcompressd_task(zram);
-create_akcompressd_task_fail:
 #endif
 #ifdef CONFIG_HYBRIDSWAP_SWAPD
 	swapd_exit();
@@ -961,10 +908,6 @@ static void hybridswap_disable(struct zram * zram)
 
 #ifdef CONFIG_HYBRIDSWAP_CORE
 	hybridswap_core_disable();
-#endif
-
-#ifdef CONFIG_HYBRIDSWAP_ASYNC_COMPRESS
-	destroy_akcompressd_task(zram);
 #endif
 
 #ifdef CONFIG_HYBRIDSWAP_SWAPD
@@ -1045,10 +988,6 @@ int __init hybridswap_pre_init(void)
 	}
 #endif
 
-#ifdef CONFIG_HYBRIDSWAP_ASYNC_COMPRESS
-	akcompressd_pre_init();
-#endif
-
 #ifdef CONFIG_HYBRIDSWAP_SWAPD
 	swapd_pre_init();
 #endif
@@ -1062,9 +1001,6 @@ int __init hybridswap_pre_init(void)
 fail_out:
 #ifdef CONFIG_HYBRIDSWAP_SWAPD
 	swapd_pre_deinit();
-#endif
-#ifdef CONFIG_HYBRIDSWAP_ASYNC_COMPRESS
-	akcompressd_pre_deinit();
 #endif
 error_out:
 	if (hybridswap_cache) {
@@ -1080,9 +1016,6 @@ void __exit hybridswap_exit(void)
 
 #ifdef CONFIG_HYBRIDSWAP_SWAPD
 	swapd_pre_deinit();
-#endif
-#ifdef CONFIG_HYBRIDSWAP_ASYNC_COMPRESS
-	akcompressd_pre_deinit();
 #endif
 
 	if (hybridswap_cache) {
