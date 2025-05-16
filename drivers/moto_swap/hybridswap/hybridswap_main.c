b5a65a556b817f568dbd8728d4c930aef5365dd1
prabhatKrMishra <mprabhat774@gmail.com>
2025-05-16 10:25:00 +0000

    moto_swap: Fix data argument not used by format string
    Signed-off-by: prabhatKrMishra <mprabhat774@gmail.com>
    Signed-off-by: 0xA11CE613 <mprabhat774@gmail.com>

--- drivers/moto_swap/hybridswap/hybridswap_main.c
+++ drivers/moto_swap/hybridswap/hybridswap_main.c
@@ -504,10 +504,10 @@ static ssize_t mem_cgroup_force_shrink_anon(struct kernfs_open_file *of,
 	else
 		nr_need_reclaim = memcg_inactive_anon_pages(memcg);
 
-	hybp(HYB_DEBUG,, "FORCE SHRINK +\n");
+	hybp(HYB_DEBUG, "FORCE SHRINK +\n");
 	nr_reclaimed = try_to_free_mem_cgroup_pages(memcg, nr_need_reclaim,
 			GFP_KERNEL, true);
-	hybp(HYB_DEBUG,, "FORCE SHRINK - to_reclaim %lu reclaimed %lu\n", nr_need_reclaim, nr_reclaimed);
+	hybp(HYB_DEBUG, "FORCE SHRINK - to_reclaim %lu reclaimed %lu\n", nr_need_reclaim, nr_reclaimed);
 	return nbytes;
 }
 
