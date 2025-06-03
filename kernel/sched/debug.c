a44ad0db663212f794057b743f6c1f926a0aee64
Peter Zijlstra <peterz@infradead.org>
2025-06-03 10:25:00 +0000

    BACKPORT: sched/fair: Commit to EEVDF
    EEVDF is a better defined scheduling policy, as a result it has less
    heuristics/tunables. There is no compelling reason to keep CFS around.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Link: https://lore.kernel.org/r/20230531124604.137187212@infradead.org
    Signed-off-by: prabhatKrMishra <mprabhat774@gmail.com>
    Signed-off-by: prabhatkrmishra <mprabhat774@gmail.com>

--- kernel/sched/debug.c
+++ kernel/sched/debug.c
@@ -177,9 +177,6 @@ static __init int sched_init_debug(void)
 	debugfs_create_bool("sched_debug", 0644, NULL,
 			&sched_debug_enabled);
 
-	debugfs_create_u32("idle_min_granularity_ns", 0644, NULL,
-			&sysctl_sched_idle_min_granularity);
-
 	return 0;
 }
 late_initcall(sched_init_debug);
@@ -754,10 +751,7 @@ static void sched_debug_header(struct seq_file *m)
 	SEQ_printf(m, "  .%-40s: %Ld\n", #x, (long long)(x))
 #define PN(x) \
 	SEQ_printf(m, "  .%-40s: %Ld.%06ld\n", #x, SPLIT_NS(x))
-	PN(sysctl_sched_latency);
 	PN(sysctl_sched_min_granularity);
-	PN(sysctl_sched_idle_min_granularity);
-	PN(sysctl_sched_wakeup_granularity);
 	P(sysctl_sched_child_runs_first);
 	P(sysctl_sched_features);
 #ifdef CONFIG_SCHED_WALT
