ad981bb8a8cb3337b276b15760ec7e842a4dddaf
Uladzislau Rezki (Sony) <urezki@gmail.com>
2025-06-10 19:34:48 +0200

    rcu: Return early if callback is not specified
    [ Upstream commit 33b6a1f155d627f5bd80c7485c598ce45428f74f ]
    
    Currently the call_rcu() API does not check whether a callback
    pointer is NULL. If NULL is passed, rcu_core() will try to invoke
    it, resulting in NULL pointer dereference and a kernel crash.
    
    To prevent this and improve debuggability, this patch adds a check
    for NULL and emits a kernel stack trace to help identify a faulty
    caller.
    
    Signed-off-by: Uladzislau Rezki (Sony) <urezki@gmail.com>
    Reviewed-by: Joel Fernandes <joelagnelf@nvidia.com>
    Signed-off-by: Joel Fernandes <joelagnelf@nvidia.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: prabhatkrmishra <mprabhat774@gmail.com>

--- kernel/rcu/tree.c
+++ kernel/rcu/tree.c
@@ -3187,6 +3187,10 @@ __call_rcu_common(struct rcu_head *head, rcu_callback_t func, bool lazy_in)
 	if (WARN_ON_ONCE(!func))
 		return;
 
+	/* Avoid NULL dereference if callback is NULL. */
+	if (WARN_ON_ONCE(!func))
+		return;
+
 	if (debug_rcu_head_queue(head)) {
 		/*
 		 * Probable double call_rcu(), so leak the callback.
