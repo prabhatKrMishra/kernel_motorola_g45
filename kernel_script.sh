524005239e901611e6e01849a76d3df519e45628
0xA11CE613 <mprabhat774@gmail.com>
2025-06-23 10:25:00 +0000

    kernel_script: Reduce netdev_budget for  low latency
    Signed-off-by: 0xA11CE613 <mprabhat774@gmail.com>

--- kernel_script.sh
+++ kernel_script.sh
@@ -12,6 +12,9 @@ echo "2-5" > /dev/cpuset/restricted/cpus
 echo "0-7" > /dev/cpuset/h-foreground
 echo "0-6" > /dev/cpuset/h-background
 
+# Low latency for gaming
+echo 128 > /proc/sys/net/core/netdev_budget
+
 # Set default and maximum receive buffer sizes
 echo 1310720 > /proc/sys/net/core/rmem_default
 echo 8388608 > /proc/sys/net/core/rmem_max
