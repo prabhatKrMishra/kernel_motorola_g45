b338f3cdd10ed8fc2089a4d0878cb6758141830d
chiteroman <98092901+chiteroman@users.noreply.github.com>
2025-06-28 10:25:00 +0000

    fs: Update path_umount for KernelSU
    Signed-off-by: 0xA11CE613 <mprabhat774@gmail.com>

--- fs/namespace.c
+++ fs/namespace.c
@@ -1692,13 +1692,27 @@ static inline bool may_mandlock(void)
 }
 #endif
 
+#ifdef CONFIG_KSU
+/**
+ * path_mounted - check whether path is mounted
+ * @path: path to check
+ *
+ * Determine whether @path refers to the root of a mount.
+ *
+ * Return: true if @path is the root of a mount, false if not.
+ */
+static inline bool path_mounted(const struct path *path)
+{
+	return path->mnt->mnt_root == path->dentry;
+}
+
 static int can_umount(const struct path *path, int flags)
 {
 	struct mount *mnt = real_mount(path->mnt);
 
 	if (!may_mount())
 		return -EPERM;
-	if (path->dentry != path->mnt->mnt_root)
+	if (!path_mounted(path))
 		return -EINVAL;
 	if (!check_mnt(mnt))
 		return -EINVAL;
@@ -1706,6 +1720,7 @@ static int can_umount(const struct path *path, int flags)
 		return -EINVAL;
 	if (flags & MNT_FORCE && !capable(CAP_SYS_ADMIN))
 		return -EPERM;
+
 	return 0;
 }
 
@@ -1724,6 +1739,7 @@ int path_umount(struct path *path, int flags)
 	mntput_no_expire(mnt);
 	return ret;
 }
+#endif
 
 /*
  * Now umount can handle mount points as well as block devices.
