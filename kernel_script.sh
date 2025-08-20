5cfb9753638bea139697e7f64f1394ed4f7243c0
0xA11CE613 <mprabhat774@gmail.com>
2025-08-20 10:25:00 +0000

    kernel_script: Update for stable networking
    Signed-off-by: 0xA11CE613 <mprabhat774@gmail.com>

--- kernel_script.sh
+++ kernel_script.sh
@@ -16,24 +16,22 @@ echo "0-6" > /dev/cpuset/h-background
 echo 128 > /proc/sys/net/core/netdev_budget
 
 # Set default and maximum receive buffer sizes
-echo 1310720 > /proc/sys/net/core/rmem_default
-echo 8388608 > /proc/sys/net/core/rmem_max
-
-# Set default and maximum send buffer sizes
-echo 327680 > /proc/sys/net/core/wmem_default
-echo 8388608 > /proc/sys/net/core/wmem_max
+echo 262144 > /proc/sys/net/core/rmem_default
+echo 262144 > /proc/sys/net/core/wmem_default
+echo 524288 > /proc/sys/net/core/rmem_max
+echo 524288 > /proc/sys/net/core/wmem_max
 
 # Set maximum size for ancillary data and options
 echo 20480 > /proc/sys/net/core/optmem_max
 
-# Increase network device input backlog
-echo 10000 > /proc/sys/net/core/netdev_max_backlog
+# Reduce backlog so network queue doesn't bloat
+echo 500 > /proc/sys/net/core/netdev_max_backlog
 
 # Set TCP receive buffer sizes (min default max)
-echo "2097152 4194304 8388608" > /proc/sys/net/ipv4/tcp_rmem
+echo "4096 87380 512000" > /proc/sys/net/ipv4/tcp_rmem
 
 # Set TCP send buffer sizes (min default max)
-echo "262144 524288 8388608" > /proc/sys/net/ipv4/tcp_wmem
+echo "4096 65536 512000" > /proc/sys/net/ipv4/tcp_wmem
 
 # Set total TCP memory thresholds (low pressure high - in pages)
 echo "44259 59012 88518" > /proc/sys/net/ipv4/tcp_mem
@@ -51,14 +49,17 @@ echo fe > /sys/class/net/rmnet5/queues/rx-0/rps_cpus
 echo fe > /sys/class/net/rmnet6/queues/rx-0/rps_cpus
 echo fe > /sys/class/net/rmnet7/queues/rx-0/rps_cpus
 
+# Pin Wi-Fi RX to both big cores
+echo c0 > /sys/class/net/wlan0/queues/rx-0/rps_cpus
+
 # Set governor settings for CPU scaling
 echo "schedutil" > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor
 echo 500 > /sys/devices/system/cpu/cpufreq/policy0/schedutil/up_rate_limit_us
 echo 1000 > /sys/devices/system/cpu/cpufreq/policy0/schedutil/down_rate_limit_us
 
 echo "schedutil" > /sys/devices/system/cpu/cpufreq/policy6/scaling_governor
-echo 500 > /sys/devices/system/cpu/cpufreq/policy6/schedutil/up_rate_limit_us
-echo 1000 > /sys/devices/system/cpu/cpufreq/policy6/schedutil/down_rate_limit_us
+echo 200 > /sys/devices/system/cpu/cpufreq/policy6/schedutil/up_rate_limit_us
+echo 500 > /sys/devices/system/cpu/cpufreq/policy6/schedutil/down_rate_limit_us
 
 # Use 10ms polling_interval for silver and gold latfloor
 echo 50 > /sys/devices/platform/soc/soc:qcom,cpu-cpu-ddr-bw/devfreq/soc:qcom,cpu-cpu-ddr-bw/polling_interval
