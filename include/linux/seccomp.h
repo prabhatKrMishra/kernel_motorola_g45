d83fa8397a11803b49a15b1c20c971b071b32525
prabhatkrmishra <mprabhat774@gmail.com>
2025-11-20 07:36:05 +0000

    [SQUASH]: Revert KernelSU-Next
    Revert "drivers/fs: Add KernelSU hooks"
    
    This reverts commit 4a2b9ad4a110ba19ecfdc42d880e9c43226deb20.
    
    Revert "fs: Backport path_umount for KernelSu"
    
    This reverts commit 3ce0a40d6118b9199b529695ce016d01700306e3.
    
    Revert "selinux: Allow init exec ksud under nosuid"
    
    This reverts commit 1e2fe371a84fa6f2b10a1793dd8d682c6b9a88ef.
    
    Revert "BACKPORT: seccomp: add filter_count field for KernelSU"
    
    This reverts commit cedd14431c9ec6d1435825098a91e326cfb228f2.
    
    Revert "kernel: Switch to KernelSU-Next"
    
    This reverts commit 57a366eb792e818d9da6538621675424922a8537.
    
    Signed-off-by: prabhatkrmishra <mprabhat774@gmail.com>

--- include/linux/seccomp.h
+++ include/linux/seccomp.h
@@ -13,7 +13,6 @@
 
 #include <linux/thread_info.h>
 #include <asm/seccomp.h>
-#include <linux/atomic.h>
 
 struct seccomp_filter;
 /**
@@ -30,7 +29,6 @@ struct seccomp_filter;
 struct seccomp {
 	int mode;
 	struct seccomp_filter *filter;
-	atomic_t filter_count;
 };
 
 #ifdef CONFIG_HAVE_ARCH_SECCOMP_FILTER
