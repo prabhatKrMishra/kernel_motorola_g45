5e299308e072a5a8a478e5fd4f12984e8a382866
Paul E. McKenney <paulmck@kernel.org>
2026-01-25 10:25:00 +0000

    rcuperf: Change rcuperf to rcuscale
    This commit further avoids conflation of rcuperf with the kernel's perf
    feature by renaming kernel/rcu/rcuperf.c to kernel/rcu/rcuscale.c, and
    also by similarly renaming the functions and variables inside this file.
    This has the side effect of changing the names of the kernel boot
    parameters, so kernel-parameters.txt and ver_functions.sh are also
    updated.  The rcutorture --torture type was also updated from rcuperf
    to rcuscale.
    
    [ paulmck: Fix bugs located by Stephen Rothwell. ]
    Reported-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>
    Signed-off-by: prabhatkrmishra <mprabhat774@gmail.com>

--- tools/testing/selftests/rcutorture/bin/kvm.sh
+++ tools/testing/selftests/rcutorture/bin/kvm.sh
@@ -63,7 +63,7 @@ usage () {
 	echo "       --qemu-args qemu-arguments"
 	echo "       --qemu-cmd qemu-system-..."
 	echo "       --results absolute-pathname"
-	echo "       --torture rcu"
+	echo "       --torture lock|rcu|rcuscale|refscale|scf"
 	echo "       --trust-make"
 	exit 1
 }
@@ -167,13 +167,14 @@ do
 		shift
 		;;
 	--torture)
-		checkarg --torture "(suite name)" "$#" "$2" '^\(lock\|rcu\|rcuperf\)$' '^--'
+		checkarg --torture "(suite name)" "$#" "$2" '^\(lock\|rcu\|rcuscale\|refscale\|scf\)$' '^--'
 		TORTURE_SUITE=$2
 		shift
-		if test "$TORTURE_SUITE" = rcuperf
+		if test "$TORTURE_SUITE" = rcuscale || test "$TORTURE_SUITE" = refscale
 		then
-			# If you really want jitter for rcuperf, specify
-			# it after specifying rcuperf.  (But why?)
+			# If you really want jitter for refscale or
+			# rcuscale, specify it after specifying the rcuscale
+			# or the refscale.  (But why jitter in these cases?)
 			jitter=0
 		fi
 		;;
