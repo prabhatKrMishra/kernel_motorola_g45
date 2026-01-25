e288ad4dcfd64edd69a76da7eeff960386863934
Paul E. McKenney <paulmck@kernel.org>
2026-01-25 10:25:00 +0000

    rcutorture: Warn on individual rcu_torture_init() error conditions
    When running rcutorture as a module, any rcu_torture_init() issues will be
    reflected in the error code from modprobe or insmod, as the case may be.
    However, these error codes are not available when running rcutorture
    built-in, for example, when using the kvm.sh script.  This commit
    therefore adds WARN_ON_ONCE() to allow distinguishing rcu_torture_init()
    errors when running rcutorture built-in.
    
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>
    Signed-off-by: prabhatkrmishra <mprabhat774@gmail.com>

--- include/linux/torture.h
+++ include/linux/torture.h
@@ -36,6 +36,14 @@
 #define VERBOSE_TOROUT_ERRSTRING(s) \
 	do { if (verbose) pr_alert("%s" TORTURE_FLAG "!!! %s\n", torture_type, s); } while (0)
 
+#define torture_init_error(firsterr)						\
+({										\
+	int ___firsterr = (firsterr);						\
+										\
+	WARN_ONCE(!IS_MODULE(CONFIG_RCU_TORTURE_TEST) && ___firsterr < 0, "Torture-test initialization failed with error code %d\n", ___firsterr); \
+	___firsterr < 0;								\
+})
+
 /* Definitions for online/offline exerciser. */
 #ifdef CONFIG_HOTPLUG_CPU
 int torture_num_online_cpus(void);
