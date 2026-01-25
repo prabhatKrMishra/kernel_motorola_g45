92b72a5355faf2abe069674c19e4ab4deda0ce40
Marco Elver <elver@google.com>
2026-01-25 10:25:00 +0000

    kcsan: Add Kernel Concurrency Sanitizer infrastructure
    Kernel Concurrency Sanitizer (KCSAN) is a dynamic data-race detector for
    kernel space. KCSAN is a sampling watchpoint-based data-race detector.
    See the included Documentation/dev-tools/kcsan.rst for more details.
    
    This patch adds basic infrastructure, but does not yet enable KCSAN for
    any architecture.
    
    Signed-off-by: Marco Elver <elver@google.com>
    Acked-by: Paul E. McKenney <paulmck@kernel.org>
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>
    Signed-off-by: prabhatkrmishra <mprabhat774@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -31,8 +31,10 @@
 #include <linux/task_io_accounting.h>
 #include <linux/posix-timers.h>
 #include <linux/rseq.h>
+#include <linux/kcsan.h>
 #include <linux/android_kabi.h>
 #include <linux/android_vendor.h>
+
 
 /* task_struct member predeclarations (sorted alphabetically): */
 struct audit_context;
@@ -1367,6 +1369,9 @@ struct task_struct {
 
 #ifdef CONFIG_KASAN
 	unsigned int			kasan_depth;
+#endif
+#ifdef CONFIG_KCSAN
+	struct kcsan_ctx		kcsan_ctx;
 #endif
 
 #ifdef CONFIG_FUNCTION_GRAPH_TRACER
