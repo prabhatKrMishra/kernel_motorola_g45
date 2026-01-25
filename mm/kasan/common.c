80ef05e22e6125815c5206dd1571123ac8d1c5ea
Walter Wu <walter-zh.wu@mediatek.com>
2026-01-25 10:25:00 +0000

    rcu: kasan: record and print call_rcu() call stack
    Patch series "kasan: memorize and print call_rcu stack", v8.
    
    This patchset improves KASAN reports by making them to have call_rcu()
    call stack information.  It is useful for programmers to solve
    use-after-free or double-free memory issue.
    
    The KASAN report was as follows(cleaned up slightly):
    
    BUG: KASAN: use-after-free in kasan_rcu_reclaim+0x58/0x60
    
    Freed by task 0:
     kasan_save_stack+0x24/0x50
     kasan_set_track+0x24/0x38
     kasan_set_free_info+0x18/0x20
     __kasan_slab_free+0x10c/0x170
     kasan_slab_free+0x10/0x18
     kfree+0x98/0x270
     kasan_rcu_reclaim+0x1c/0x60
    
    Last call_rcu():
     kasan_save_stack+0x24/0x50
     kasan_record_aux_stack+0xbc/0xd0
     call_rcu+0x8c/0x580
     kasan_rcu_uaf+0xf4/0xf8
    
    Generic KASAN will record the last two call_rcu() call stacks and print up
    to 2 call_rcu() call stacks in KASAN report.  it is only suitable for
    generic KASAN.
    
    This feature considers the size of struct kasan_alloc_meta and
    kasan_free_meta, we try to optimize the structure layout and size, lets it
    get better memory consumption.
    
    [1]https://bugzilla.kernel.org/show_bug.cgi?id=198437
    [2]https://groups.google.com/forum/#!searchin/kasan-dev/better$20stack$20traces$20for$20rcu%7Csort:date/kasan-dev/KQsjT_88hDE/7rNUZprRBgAJ
    
    This patch (of 4):
    
    This feature will record the last two call_rcu() call stacks and prints up
    to 2 call_rcu() call stacks in KASAN report.
    
    When call_rcu() is called, we store the call_rcu() call stack into slub
    alloc meta-data, so that the KASAN report can print rcu stack.
    
    [1]https://bugzilla.kernel.org/show_bug.cgi?id=198437
    [2]https://groups.google.com/forum/#!searchin/kasan-dev/better$20stack$20traces$20for$20rcu%7Csort:date/kasan-dev/KQsjT_88hDE/7rNUZprRBgAJ
    
    [walter-zh.wu@mediatek.com: build fix]
      Link: http://lkml.kernel.org/r/20200710162401.23816-1-walter-zh.wu@mediatek.com
    
    Suggested-by: Dmitry Vyukov <dvyukov@google.com>
    Signed-off-by: Walter Wu <walter-zh.wu@mediatek.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Tested-by: Dmitry Vyukov <dvyukov@google.com>
    Reviewed-by: Dmitry Vyukov <dvyukov@google.com>
    Reviewed-by: Andrey Konovalov <andreyknvl@google.com>
    Acked-by: Paul E. McKenney <paulmck@kernel.org>
    Cc: Andrey Ryabinin <aryabinin@virtuozzo.com>
    Cc: Alexander Potapenko <glider@google.com>
    Cc: Josh Triplett <josh@joshtriplett.org>
    Cc: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Cc: Lai Jiangshan <jiangshanlai@gmail.com>
    Cc: Joel Fernandes <joel@joelfernandes.org>
    Cc: Jonathan Corbet <corbet@lwn.net>
    Cc: Matthias Brugger <matthias.bgg@gmail.com>
    Link: http://lkml.kernel.org/r/20200710162123.23713-1-walter-zh.wu@mediatek.com
    Link: http://lkml.kernel.org/r/20200601050847.1096-1-walter-zh.wu@mediatek.com
    Link: http://lkml.kernel.org/r/20200601050927.1153-1-walter-zh.wu@mediatek.com
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: prabhatkrmishra <mprabhat774@gmail.com>

--- mm/kasan/common.c
+++ mm/kasan/common.c
@@ -61,7 +61,7 @@ static inline unsigned int filter_irq_stacks(unsigned long *entries,
 	return nr_entries;
 }
 
-static inline depot_stack_handle_t save_stack(gfp_t flags)
+depot_stack_handle_t kasan_save_stack(gfp_t flags)
 {
 	unsigned long entries[KASAN_STACK_DEPTH];
 	unsigned int nr_entries;
@@ -74,7 +74,7 @@ static inline depot_stack_handle_t save_stack(gfp_t flags)
 static inline void set_track(struct kasan_track *track, gfp_t flags)
 {
 	track->pid = current->pid;
-	track->stack = save_stack(flags);
+	track->stack = kasan_save_stack(flags);
 }
 
 void kasan_enable_current(void)
