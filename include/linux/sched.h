5329cd044aa6c40ccf60cbb93043a019f11bf424
prabhatkrmishra <mprabhat774@gmail.com>
2026-03-01 18:59:44 +0000

    Revert "sched/walt: Improve the scheduler"
    This reverts commit 0fe8be6276933e28fd5ca11536f0e519d7250fd7.
    
    Signed-off-by: prabhatkrmishra <mprabhat774@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -571,9 +571,6 @@ extern void walt_update_cluster_topology(void);
 #define RAVG_HIST_SIZE_MAX  5
 #define NUM_BUSY_BUCKETS 10
 
-#define WALT_LOW_LATENCY_PROCFS	BIT(0)
-#define WALT_LOW_LATENCY_BINDER	BIT(1)
-
 struct walt_task_struct {
 	/*
 	 * 'mark_start' marks the beginning of an event (task waking up, task
