1179175d2dbc8c2335e227faba34431e3bd364c4
prabhatkrmishra <mprabhat774@gmail.com>
2026-05-09 21:11:51 +0700

    tree: Implement kernelsu syscall hooks
    Signed-off-by: prabhatkrmishra <mprabhat774@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1742,6 +1742,14 @@ static int exec_binprm(struct linux_binprm *bprm)
 	return ret;
 }
 
+#if defined(CONFIG_KSU)
+extern bool ksu_execveat_hook __read_mostly;
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
+			void *envp, int *flags);
+extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
+				 void *argv, void *envp, int *flags);
+#endif
+
 /*
  * sys_execve() executes a new program.
  */
@@ -1757,6 +1765,13 @@ static int __do_execve_file(int fd, struct filename *filename,
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
+
+#if defined(CONFIG_KSU)
+	if (unlikely(ksu_execveat_hook))
+		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+	else
+		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
+#endif
 
 	/*
 	 * We move the actual failure in case of RLIMIT_NPROC excess from
